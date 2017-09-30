e9ea5d79a2301d07084f977838ccb26d389a9914
Александра Суменко <aleksa.sumenko@yandex.ru>
2017-09-30 21:33:09 +0300

    second version

--- src/priceMas.java
+++ src/priceMas.java
@@ -1,40 +1,29 @@
-import java.util.Arrays;
-
-class priceMas {
-    static int[] subMas(int Price[]) {
-        int Delta[] = new int[Price.length - 1];
-        for (int i = 0; i < Price.length - 1; i++) {
-            Delta[i] = Price[i + 1] - Price[i];
-        }
-        return Delta;
-    }
-
-    static void prn(int a[]) {
-        System.out.println(Arrays.toString(a));
-    }
-
-    static void searchSubmas(int Price[], int Delta[]) {
+public class PriceMas {
+    public int[] searchSubmas(int price[]) {
         int maxSum = 0;
         int curSum = 0;
         int begin = 0;
         int end = 0;
-        int a = 0;
-        for (int i = 0; i < Delta.length; i++) {
-            curSum += Delta[i];
+        int startPoint = -1;
+        int k = 0;
+        for (int i = 0; i < price.length; i++) {
+            curSum += price[i];
             if (curSum > maxSum) {
                 maxSum = curSum;
-                begin = a + 1;
+                begin = startPoint + 1;
                 end = i;
             }
 
             if (curSum < 0) {
                 curSum = 0;
-                a = i;
+                startPoint = i;
             }
         }
-        for (int i = begin; i <= end + 1; i++)
-            System.out.print(Price[i] + " ");
-        System.out.println();
-        System.out.println(maxSum);
+        int result[] = new int[end - begin + 1];
+        for (int i = begin; i <= end; i++) {
+            result[k] = price[i];
+            k++;
+        }
+        return result;
     }
 }
